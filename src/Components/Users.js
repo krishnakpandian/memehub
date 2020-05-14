d690d98f615a23d18a9b238c3b9f01ac3a0bb235
kpandian <kpandian@ucsc.edu>
2020-05-14 16:50:44 -0700

    Made some additions

---
+++ src/Components/Users.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import '../index.css';
+import { render } from '@testing-library/react';
+
+class Users extends Component {
+  state = {
+    results: [],
+  };
+
+  async componentDidMount() { //Fetches the data from the api
+    const url = 'https://randomuser.me/api/';
+    fetch(url)
+    .then(response => response.json())    // one extra step
+    .then(data => {
+      console.log(data) 
+      this.setState({results: data})
+    })
+    .catch(error => console.error(error));
+  }
+
+  render() {
+      return (
+      <React.Fragment>
+        <div> Hello World</div>
+      </React.Fragment>
+      );
+    }
+}
+
+export default Users;
