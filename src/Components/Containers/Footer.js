b95049c4e5be2348f35b46f1ae9424f49b0e430f
alkngo <alexngo2011@hotmail.com>
2020-06-10 20:37:54 -0700

    Social Media, Copyright

--- src/Components/Containers/Footer.js
+++ src/Components/Containers/Footer.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react'
+import './Footer.css';
+
 
 class Footer extends Component {
     render() {
       return (
         <React.Fragment>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <footer>
             <b>Contact Us</b>
             <br />
-            <a href="mailto:" target="_blank" rel="noopener noreferrer"><span>Krishna</span></a>
-            <a href="mailto:" target="_blank" rel="noopener noreferrer"><span>Alex</span></a>
+            <div>
+              <a href="mailto:" target="_blank" rel="noopener noreferrer"><span>Krishna</span></a>
+            
+              <a href="#" class="fa fa-linkedin"></a>
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+            </div>
+            <div>
+              <a href="mailto:" target="_blank" rel="noopener noreferrer"><span>Alex</span></a>
+
+              <a href="#" class="fa fa-linkedin"></a>
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+            </div>
+            <p>  © 2020 ??? inc. All rights reserved.</p>
         </footer>
         </React.Fragment>
       );
