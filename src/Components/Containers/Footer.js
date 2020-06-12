fadd38282302e5f809ff12d62a3bbbc42cc70cef
alkngo <alexngo2011@hotmail.com>
2020-06-12 16:44:04 -0700

    final fix after merge conflict

--- src/Components/Containers/Footer.js
+++ src/Components/Containers/Footer.js
@@ -1,29 +1,18 @@
 import React, {Component} from 'react'
-<<<<<<< HEAD
-import './Footer.css';
-=======
 import './Footer.scss';
->>>>>>> 1de4090ce76b467835ba026a3cc4514bf08935c1
 
 class Footer extends Component {
     render() {
       return (
         <React.Fragment>
-<<<<<<< HEAD
-=======
         <div class="footer-container">
->>>>>>> 1de4090ce76b467835ba026a3cc4514bf08935c1
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <footer>
             <b>Contact Us</b>
             <br />
             <div>
               <a href="mailto:" target="_blank" rel="noopener noreferrer"><span>Krishna</span></a>
-<<<<<<< HEAD
-            
-=======
            
->>>>>>> 1de4090ce76b467835ba026a3cc4514bf08935c1
               <a href="https://www.linkedin.com/" class="fa fa-linkedin"> </a>
               <a href="https://www.facebook.com/" class="fa fa-facebook"> </a>
               <a href="https://twitter.com/" class="fa fa-twitter"> </a>
@@ -42,10 +31,5 @@ class Footer extends Component {
       );
     }
   }
-<<<<<<< HEAD
-  
-export default Footer; 
-=======
  
 export default Footer; 
->>>>>>> 1de4090ce76b467835ba026a3cc4514bf08935c1
