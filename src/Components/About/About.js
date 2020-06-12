a0d5410086f87b3f6e840ab8528710fa8ddc40e7
alkngo <alexngo2011@hotmail.com>
2020-06-12 16:40:13 -0700

    Worked on About

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,18 +1,22 @@
 import React, {Component} from 'react'
+import './About.scss';
+
 
 class About extends Component {
     render() {
       return (
         <React.Fragment>
-          <h1>
-            Welcome to Meme-Hub (title pending)!
-          </h1>
-          <p>
-            We use cutting edge technology to deliver you the best meme-sharing service!
-          </p>
-          <p>
-            Join our userbase and enjoy the comforts of a salubrious meme-sharing community.
-          </p>
+          <div class="about-container">
+            <h1>
+              Welcome to Meme-Hub (title pending)!
+            </h1>
+            <p>
+              We use cutting edge technology to deliver you the best meme-sharing service!
+            </p>
+            <p>
+              Join our userbase and enjoy the comforts of a salubrious meme-sharing community.
+            </p>
+          </div>
         </React.Fragment>
       );
     }
